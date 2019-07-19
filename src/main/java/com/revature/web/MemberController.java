4dac18f1a657176cc697e91d2de45a28c3effaf8
vicente0924 <vicente0924@gmail.com>
2019-07-19 11:45:29 -0500

    Testing cross origin

--- src/main/java/com/revature/web/MemberController.java
+++ src/main/java/com/revature/web/MemberController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.revature.Service.MemberServices;
 import com.revature.model.Member;
 
+@CrossOrigin(origins="/*")
 @RestController
 @RequestMapping(value="/PlusUltraApi")
 public class MemberController
