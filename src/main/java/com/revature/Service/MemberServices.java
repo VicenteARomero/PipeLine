a69e14845d9f5ec9cb6a70edc129b013b4bacdc6
vicente0924 <vicente0924@gmail.com>
2019-07-21 15:52:11 -0500

    added a method to service that will ping a url and then ping a second url to get desired data

--- src/main/java/com/revature/Service/MemberServices.java
+++ src/main/java/com/revature/Service/MemberServices.java
@@ -2,12 +2,17 @@ package com.revature.Service;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.revature.model.Files;
 import com.revature.model.Member;
 import com.revature.repository.MemberRepo;
 import com.revature.web.MemberController;
@@ -30,9 +35,9 @@ public class MemberServices
 		this.mr=mr;
 	}
 	
+	//this method validates login
 	public ResponseEntity<Object> validate(Member m)
 	{
-		getDataList();
 		
 		if(m.getUsername().equals("super2") && m.getPassword().equals("pass"))
 		{
@@ -44,15 +49,40 @@ public class MemberServices
 		}
 	}
 	
+	//This method will trigger every hour and update a json file stored in the database
 	private static void getDataList()
 	{
-	    final String uri = "https://us.api.blizzard.com/wow/auction/data/arthas?locale=en_US&access_token=USc0dB2Pb9yH8ucNlYpJL1anH2wo68EPO5";
-	     
+	    final String uri = "https://us.api.blizzard.com/wow/auction/data/arthas?locale=en_US&access_token=USc0dB2Pb9yH8ucNlYpJL1anH2wo68EPO5"; 
 	    RestTemplate restTemplate = new RestTemplate();
 	    String result = restTemplate.getForObject(uri, String.class);
-	     
 	    System.out.println(result);
+	    
+	    JSONParser parse;
+	    JSONObject jobj; 
+	    JSONArray jsonarr_1;
+	    String url = null;
+
+	    try
+		{
+	    	parse = new JSONParser();
+			jobj = (JSONObject)parse.parse(result);
+			jsonarr_1 = (JSONArray) jobj.get("files");
+			for(int i=0;i<jsonarr_1.size();i++)
+			{
+				JSONObject jsonobj_1 = (JSONObject)jsonarr_1.get(i);
+				url = (String)jsonobj_1.get("url");
+				System.out.println(url);
+				String itemList = restTemplate.getForObject(url, String.class);
+			    System.out.println(itemList);
+			}
+			
+		} catch (ParseException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
 	}
+	
 	
 	
 }
--- src/main/java/com/revature/web/MemberController.java
+++ src/main/java/com/revature/web/MemberController.java
@@ -1,23 +1,18 @@
 package com.revature.web;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.Service.MemberServices;
-import com.revature.aspect.LoggingAspect;
 import com.revature.model.Member;
 
 @CrossOrigin
@@ -41,4 +36,5 @@ public class MemberController
 	{
 		return ms.validate(m);
 	}
+	
 }
